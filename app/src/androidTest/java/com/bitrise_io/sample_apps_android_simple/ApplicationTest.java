ee7af8a3d214195185b062d38258ce9b48317052
Tamás Bazsonyi <tamas.bazsonyi@gmail.com>
2015-10-22 18:22:13 +0200

    updated .gitignore (+1 squashed commit) Squashed commits: [a22a70b] Added sample app

---
+++ app/src/androidTest/java/com/bitrise_io/sample_apps_android_simple/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.bitrise_io.sample_apps_android_simple;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
